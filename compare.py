d810d0fd677eedf7ca1c480e595d588236f722a7
Nicholas Angeli <nicangeli@gmail.com>
2013-04-22 12:45:34 +0100

    Changes mutation rate dynamically for each of the mutations, each 100000 times

--- compare.py
+++ compare.py
@@ -1,39 +1,82 @@
-#! /usr/bin/env python
+#!/usr/bin/env python3
+""" Python module for fitness calculations according to http://www.linguistics.ucla.edu/people/stabler/LeeEtAl2005Grammar.pdf """
+
+#These modifications put stuff in functions for easier reuse.
+
 import itertools
+import numpy
+import random
 
-def compareTwoLanguages(l1, l2):
-	count = 0;
-	for index in range(0, LANGUAGE_LENGTH):
-		if(l1[index] == l2[index]):
-			count += 1;
-	return (float(count)/LANGUAGE_LENGTH);
+def similarity_measure(language1, language2):
+    """Calculate the language similarity for two languages.
 
-def buildLanguages():
-	cartesian = itertools.product(CHARACTERS_IN_LANGUAGE, repeat=LANGUAGE_LENGTH)
-	l = []
-	for c in cartesian:
-		l.append("".join(c))
-	return l
+    Representing a language as a sequence, the similarity between
+    languages i and j, denoted a_ij, is defined as the proportion of
+    parameters on which the two individuals agree. For example, the
+    language similarity between an individual i whose language is
+    represented as AAA and an individual j whose language is represented
+    as ABA is a_ij=2/3."""
 
-LANGUAGE_LENGTH = 2;
-CHARACTERS_IN_LANGUAGE = ['A', 'B'];
-languages = buildLanguages(); #length of this list will be the CHARACTERS_IN_LANGUGE ^ LANGUAGE_LENGTH
+    # The following line also accepts an array of languages for
+    # language2. This is not obvious, but it works, and I'll explain
+    # the details next time we meet, if you want me to.
+    return (language1==language2).sum()/len(language1)
+    
+def fitness(language, population, f0=1e-3):
+    """Calculate the fitness of a language in a given population.
 
-for l in range(len(languages)):
-	#l is the element we're testing
-	#we want to get the rest of the elements in the array
-	other_elements = languages[:];
-	del other_elements[l];
-	print('Comparing ' + languages[l]  + ' to other languages: ')
-	coefficients = []
-	sum = 0;
-	for element in other_elements:
-		tmp = compareTwoLanguages(languages[l], element)
-		coefficients.append(tmp);
-		sum += tmp
-	print(coefficients);
-	print sum
+    The overall fitness of an individual, f_i, is described as
+
+    f_i = f_0 + sum_j a_ij
+
+    with a_ij calculated using similarity_measure"""
+
+    return f0 + similarity_measure(
+        language,
+        numpy.asarray(population)) / len(population)
+
+def one_each_population(length, n_values):
+    """Generate a population that contains every language precisely once.
+    """
+    population=[]
+
+    all_languages = itertools.product(
+        range(n_values),
+        repeat=lengthofsequence)
+    for pairing in all_languages: 
+        population.append(list(pairing))
+
+    return population
+
+def mutation(population,lengthofsequence,numberofvalues,mutationrate):
+    index1=random.randrange(0,len(population))
+    index2=random.randrange(0,len(population))
+    population[index1]=population[index2]
+    randomnumber=random.random()
+    if randomnumber<mutationrate:        
+        parametertochange=random.randrange(0,lengthofsequence)
+        valuetochange=random.randrange(0,numberofvalues)
+        population[index2][parametertochange]=valuetochange
+
+    return population
+
+
+# Simulation parameters
+lengthofsequence = 1
+numberofvalues = 64
 
 
 
+for y in numpy.arange(0.5, 1.02, 0.02): 
+    population = one_each_population(lengthofsequence, numberofvalues)
 
+    for x in range(0,1000):
+        population=mutation(population,lengthofsequence,numberofvalues,y)
+    for language in population:
+        print (language)
+    for language in population:
+        f = fitness(language, population)
+        print("{0} = {1}".format(language, f))
+
+
+    print(y)
